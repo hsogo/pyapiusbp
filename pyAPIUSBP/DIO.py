929d47bf403dc63615549a9d9f29aec32a53b8eb
Hiroyuki Sogo <hsogo12600@gmail.com>
2017-05-24 21:13:24 +0900

    FIX: add argtypes to DLL functions

--- pyAPIUSBP/DIO.py
+++ pyAPIUSBP/DIO.py
@@ -235,6 +235,37 @@ DIO_ERR_DM_LOCK_MEMORY				= 21101	# Memory has not been locked.
 DIO_ERR_DM_PARAM					= 21102	# Parameter error
 DIO_ERR_DM_SEQUENCE					= 21103	# Procedure error of execution
 
+# prototype
+DLL.DioInit.argtypes = [ctypes.POINTER(ctypes.c_char), ctypes.POINTER(ctypes.c_short)]
+DLL.DioExit.argtypes = [ctypes.c_short]
+DLL.DioResetDevice.argtypes = [ctypes.c_short]
+DLL.DioGetErrorString.argtypes = [ctypes.c_long, ctypes.POINTER(ctypes.c_char)]
+DLL.DioSetDigitalFilter.argtypes = [ctypes.c_short, ctypes.c_short]
+DLL.DioGetDigitalFilter.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_short)]
+DLL.DioSetIoDirection.argtypes = [ctypes.c_short, ctypes.c_uint]
+DLL.DioGetIoDirection.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_uint)]
+DLL.DioSetIoDirectionEx.argtypes = [ctypes.c_short, ctypes.c_uint]
+DLL.DioGetIoDirectionEx.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_uint)]
+DLL.DioSet8255Mode.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.c_short]
+DLL.DioGet8255Mode.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.POINTER(ctypes.c_short)]
+DLL.DioInpByte.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioInpBit.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioOutByte.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.c_ubyte]
+DLL.DioOutBit.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.c_ubyte]
+DLL.DioEchoBackByte.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioEchoBackBit.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioInpMultiByte.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_short), ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioInpMultiBit.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_short), ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioOutMultiByte.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_short), ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioOutMultiBit.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_short), ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioEchoBackMultiByte.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_short), ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioEchoBackMultiBit.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_short), ctypes.c_short, ctypes.POINTER(ctypes.c_ubyte)]
+DLL.DioQueryDeviceName.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_char), ctypes.POINTER(ctypes.c_char)]
+DLL.DioGetDeviceType.argtypes = [ctypes.POINTER(ctypes.c_char), ctypes.POINTER(ctypes.c_short)]
+DLL.DioGetMaxPorts.argtypes = [ctypes.c_short, ctypes.POINTER(ctypes.c_short), ctypes.POINTER(ctypes.c_short)]
+DLL.DioSetDemoByte.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.c_ubyte]
+DLL.DioSetDemoBit.argtypes = [ctypes.c_short, ctypes.c_short, ctypes.c_ubyte]
+
 
 def queryDioDeviceName():
     """
